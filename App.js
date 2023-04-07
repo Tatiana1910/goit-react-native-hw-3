4348e0d8c7408693f7f374d1a066ec2607a640a4
Tatiana Kramarenko <tanya19101996@gmail.com>
2023-04-07 13:45:43 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, View } from "react-native";
+// import LoginScreen from "./src/screens/LoginScreen";
+import RegistrationScreen from "./src/screens/RegistrationScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
